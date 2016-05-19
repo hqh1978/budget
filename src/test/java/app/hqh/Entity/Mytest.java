fbbbeb55faade748d26bde99158555be4fe27ae4
hqh1978 <hqh1978@163.com>
2016-05-19 12:34:57 -0400

    budget initialized

---
+++ src/test/java/app/hqh/Entity/Mytest.java
@@ -0,0 +1,11 @@
+package app.hqh.Entity;
+
+import org.springframework.context.ApplicationContext;
+
+public class Mytest {
+	public static void main(String args[]) {
+		ApplicationContext context = null;
+		String ttt = "file:webapp/WEB-INF/config/";
+
+	}
+}
